dec0f648a85461b2e555b72a9ce76c8adc0069bb
jay2452 <jay2452@live.com>
2016-08-25 13:35:27 +0530

    hide feedbacks

--- app/models/faculty_subject.rb
+++ app/models/faculty_subject.rb
@@ -10,13 +10,20 @@ class FacultySubject < ApplicationRecord
       # if row["code"].empty? == false
         # subject = find_by_code(row["code"])
       # else
-      fac_id = Faculty.where("emp_id = ? AND name = ?", row["emp_id"], row["name"]).first
+      fac_id = Faculty.where("name = ?", row["name"]).first
       sub_id = Subject.find_by_code(row["subject code"])
 
+      puts "++++++++++++++++"
+        p row["emp id"]
+        p row["name"]
+        p fac_id.id
+        p sub_id.id
+      puts "++++++++++++++++"
 
-      fac_sub = where("faculty_id = ? AND subject_id = ?",fac_id,  sub_id) || FacultySubject.new
-      fac_sub.faculty_id = fac_id
-      fac_sub.subject_id = sub_id
+
+      fac_sub = where("faculty_id = ? AND subject_id = ?",fac_id.id,  sub_id.id).first || FacultySubject.new
+      fac_sub.faculty_id = fac_id.id
+      fac_sub.subject_id = sub_id.id
       fac_sub.save!
      end # => end of do
  end # => end of def
