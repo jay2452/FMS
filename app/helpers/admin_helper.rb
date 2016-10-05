e20cef24231adedff8ddee85f9a219c5a98172f9
jay2452 <jay2452@live.com>
2016-10-05 20:48:33 +0530

    feedback queries

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,12 @@
 module AdminHelper
+
+  def users_not_given_feedback
+    arr = []
+    UserRating.select('user_id').distinct.each do |user|
+      arr << user.user_id
+    end
+
+    User.all - User.where("id IN (?)", arr)
+  end
+
 end
